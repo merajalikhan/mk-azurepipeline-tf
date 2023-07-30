cb72a7eabf03248c329ef2b8afc3965bd0a7cf27
Meraj Khan <merajakhan@hotmail.com>
2023-07-30 19:55:02 +0100

    updated tf file

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -19,15 +19,10 @@ provider "azurerm" {
   skip_provider_registration = true
 }
 
-resource "azurerm_resource_group" "example" {
-  name     = "example-resources"
-  location = "West Europe"
-}
-
-resource "azurerm_virtual_network" "example" {
-  name                = "example-network"
+resource "azurerm_virtual_network" "mkvn" {
+  name                = "mk-network"
   address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.mkrg.location
+  resource_group_name = mk-space-game-rg
 }
 

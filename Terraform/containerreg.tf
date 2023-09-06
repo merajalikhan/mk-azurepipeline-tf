5fe713113459c4c138938f764b25365606ae4298
merajalikhan <104003533+merajalikhan@users.noreply.github.com>
2023-09-06 12:34:37 +0100

    Update containerreg.tf

--- Terraform/containerreg.tf
+++ Terraform/containerreg.tf
@@ -1,6 +1,6 @@
 resource "azurerm_container_registry" "acr" {
   name                =  "makscr123"#"${var.prefix}acr${lower(random_id.random_number.hex)}"  #"${var.prefix}acr"
-  resource_group_name = 'mk-space-game-rg' #var.rgname #azurerm_resource_group.devopsjourney_resource_group.name
-  location            =  'UK South' #var.location #azurerm_resource_group.devopsjourney_resource_group.location
-  sku                 = "Standard"
-  admin_enabled       = false
+  resource_group_name =  "mk-space-game-rg"
+  location            =  "UK South" 
+  sku                 =  "Standard"
+  admin_enabled       =  false
